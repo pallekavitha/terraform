d4471a2a7b67d4b946b32c15fb30db067f633315
pallekavitha <pallewarkavitha007@gmail.com>
2023-09-23 23:01:00 +0530

    terraform

---
+++ count/variables.tf
@@ -0,0 +1,16 @@
+variable "ami_id" {
+   default = "ami-03265a0778a880afb"
+}
+
+variable "instance_names" {
+    type = list
+    default = ["MongoDB", "Cart", "Redis", "Catalogue", "User", "MySQL", "RabbitMQ", "Shipping", "Payment", "Web"]
+}
+
+variable "zone_id" {
+    default = "Z00055312F8QWOU888ABK"
+}
+
+variable "domain" {
+    default = "joindevops.live"
+}
